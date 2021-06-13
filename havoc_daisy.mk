792a6fa85fc6cac93776a8f38aaf8fdec6b0ddc1
idelfonsotutors <45271413+IdelfonsoTutors@users.noreply.github.com>
2021-06-13 11:51:38 -0300

    Update and rename aosip_daisy.mk to havoc_daisy.mk

--- havoc_daisy.mk
+++ havoc_daisy.mk
@@ -28,14 +28,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
 # Inherit some common Potato sauce.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit some prebuilts
 $(call inherit-product, vendor/custom/prebuilts/config.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
-PRODUCT_NAME := aosip_daisy
+PRODUCT_NAME := havoc_daisy
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A2 Lite
