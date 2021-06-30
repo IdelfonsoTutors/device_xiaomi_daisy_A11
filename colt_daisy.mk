ac07d41ee51f189225b0727f2f38055fe92f9266
idelfonsotutors <45271413+IdelfonsoTutors@users.noreply.github.com>
2021-06-30 08:30:28 -0300

    Update and rename aosp_daisy.mk to colt_daisy.mk

--- colt_daisy.mk
+++ colt_daisy.mk
@@ -28,14 +28,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
 # Inherit some common Potato sauce.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
+COLT_BUILD_MAINTAINER := Idel
 
 # Inherit some prebuilts
 $(call inherit-product, vendor/custom/prebuilts/config.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
-PRODUCT_NAME := aosp_daisy
+PRODUCT_NAME := colt_daisy
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A2 Lite
@@ -61,8 +62,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# PE
-CUSTOM_BUILD_TYPE=IDEL
+# Custom
+CUSTOM_BUILD_TYPE=MONTHLY
 
 # Use MiuiCamera
 WITH_MIUICAM := true
