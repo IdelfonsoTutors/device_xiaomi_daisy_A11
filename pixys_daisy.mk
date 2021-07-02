52c488e39573b980ca9382b20a7c68147f3be70a
idelfonsotutors <45271413+IdelfonsoTutors@users.noreply.github.com>
2021-07-01 21:55:48 -0300

    Create pixys_daisy.mk

--- pixys_daisy.mk
+++ pixys_daisy.mk
@@ -28,14 +28,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
 # Inherit some common Potato sauce.
-$(call inherit-product, vendor/wave/config/common.mk)
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 # Inherit some prebuilts
 $(call inherit-product, vendor/custom/prebuilts/config.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
-PRODUCT_NAME := wave_daisy
+PRODUCT_NAME := pixys_daisy
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A2 Lite
